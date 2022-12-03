925431e51ec7496b261cbf02b475b6e1461bc6ee
Alaa Albayed <31359882+alaaalbayed@users.noreply.github.com>
2022-12-03 18:03:44 +0300

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,9 +4,9 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || "!", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/seRXpNZ7", // Support Server Link
-  Token: process.env.Token || "OTk2Mjk5NDc0MjY1NzE0NzY4.Ggh5zk.-wvQ78Yo2XTWFjEQL02kDgcCYajxBb2SmL8phI", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "996299474265714768", // Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "Cg4J0zF1CB3RqZR7Rky_rGNV6wqEOxX0", // Discord Client Secret
+  Token: process.env.Token || "", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "", // Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
   ServerDeafen: true, // If you want bot to stay deafened
   DefaultVolume: 100, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
